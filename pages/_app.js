b3d33d31f17dc46cc1fd8ae975dbfdc703d4002f
Vinit Kurmude <vkurmude14@gmail.com>
2022-05-23 21:47:48 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 function MyApp({ Component, pageProps }) {
   // console.log("I am _app.js")
   return <>
